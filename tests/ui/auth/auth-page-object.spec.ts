54c677d93b1c6b31a94f5f8d297498bc2f449619
Alexey <agudz@evolution.com>
2025-07-14 15:24:38 +0300

    Remove banned "only"

--- tests/ui/auth/auth-page-object.spec.ts
+++ tests/ui/auth/auth-page-object.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 import { LoginPage } from '../../pages/login-page'
 import { OrderPage } from '../../pages/order-page'
 
-test.only('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
+test('Sign in button is disabled when an invalid username is entered', async ({ page }) => {
   const authPage = new LoginPage(page)
   await authPage.open()
   const OrderPage: OrderPage = await authPage.signIn()
